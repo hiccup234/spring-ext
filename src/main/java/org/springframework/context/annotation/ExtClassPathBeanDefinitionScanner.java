274d364657a5d36ef0ad7cbf74cba78425dc7d95
hiccup <291746192@qq.com>
2020-02-17 18:39:09 +0800

    spring ext Replace

--- src/main/java/org/springframework/context/annotation/ExtClassPathBeanDefinitionScanner.java
+++ src/main/java/org/springframework/context/annotation/ExtClassPathBeanDefinitionScanner.java
@@ -1,7 +1,21 @@
+/*
+ * Copyright 2002-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.context.annotation;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanNameGenerator;
@@ -39,14 +53,12 @@ public class ExtClassPathBeanDefinitionScanner extends ClassPathBeanDefinitionSc
             classLoader = this.getClass().getClassLoader();
         }
         Class clazz = Class.forName(beanClassName, true, classLoader);
-        Replace replace = (Replace)AnnotationUtils.findAnnotation(clazz, Replace.class);
+        Replace replace = (Replace) AnnotationUtils.findAnnotation(clazz, Replace.class);
         return replace;
     }
 
     @Override
-    protected boolean checkCandidate(String beanName, BeanDefinition beanDefinition)
-            throws IllegalStateException
-    {
+    protected boolean checkCandidate(String beanName, BeanDefinition beanDefinition) throws IllegalStateException {
         if (!getRegistry().containsBeanDefinition(beanName)) {
             return true;
         }
@@ -63,25 +75,24 @@ public class ExtClassPathBeanDefinitionScanner extends ClassPathBeanDefinitionSc
         {
             Replace replace = getReplaceAnnotation(beanDefinition.getBeanClassName());
             if (replace != null) {
-                if (this.logger.isInfoEnabled()) {
-                    this.logger.info("替换组件名称[" + beanName + "],新对象[" + beanDefinition
-                            .getBeanClassName() + "],旧对象[" + existingDef.getBeanClassName() + "].");
+                if (this.logger.isWarnEnabled()) {
+                    this.logger.warn("组件名称【" + beanName + "】，本地组件类名【" + beanDefinition
+                            .getBeanClassName() + "】替换核心组件类名【" + existingDef.getBeanClassName() + "】");
                 }
                 return true;
             }
 
             replace = getReplaceAnnotation(existingDef.getBeanClassName());
             if (replace != null) {
-                if (this.logger.isInfoEnabled()) {
-                    this.logger.info("本地化组件名称[" + beanName + "]已加载[" + existingDef
-                            .getBeanClassName() + "],忽略加载对象[" + beanDefinition.getBeanClassName() + "].");
+                if (this.logger.isWarnEnabled()) {
+                    this.logger.warn("组件名称【" + beanName + "】，已加载本地组件类名【" + existingDef
+                            .getBeanClassName() + "】，忽略核心组件的加载【" + beanDefinition.getBeanClassName() + "】");
                 }
                 return false;
             }
 
-        }
-        catch (ClassNotFoundException localClassNotFoundException)
-        {
+        } catch (ClassNotFoundException e) {
+            logger.error("ExtClassPathBeanDefinitionScanner ClassNotFoundException: ", e);
         }
 
         throw new IllegalStateException("Annotation-specified bean name '" + beanName + "' for bean class [" + beanDefinition
