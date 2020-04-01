28cb667bc9b8dd0ef4735ff3fad4a191dec04d6d
hiccup <291746192@qq.com>
2020-04-01 19:17:55 +0800

    Replace

--- src/main/java/org/springframework/context/annotation/Replace.java
+++ src/main/java/org/springframework/context/annotation/Replace.java
@@ -1,22 +1,23 @@
 package org.springframework.context.annotation;
 
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
 import java.lang.annotation.*;
 
 /**
  * Replace注解：
- * 在Spring启动加载Bean时用被@Replace标注的组件替换容器中原来的同名组件
+ * 在Spring启动加载Bean时，被@Replace标注的组件替换容器中原来的同名组件
  *
  * @author wenhy
  * @date 2018/5/31
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
+@Component
 @Documented
 public @interface Replace {
 
-    /**
-     * 多个继承子类时，以order值最大的作为最终注册到容器的bean
-     * @return
-     */
-    String order() default "0";
+    @AliasFor(annotation = Component.class)
+    String value();
 }
