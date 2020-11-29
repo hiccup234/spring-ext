f0239ac8e3ae80063b0c52b643c4c0144cb86947
hiccup234 <wenhaiyang3@jd.com>
2020-11-29 21:22:58 +0800

    spring test

--- src/test/java/top/hiccup/spring/test/circular/MainTest.java
+++ src/test/java/top/hiccup/spring/test/circular/MainTest.java
@@ -14,7 +14,7 @@ public class MainTest {
 
     @Test
     public void root() {
-        // 构造器
+        // 构造器（Spring无法解决构造器循环依赖的问题）
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/test/circular/root.xml");
         StudentA a = (StudentA) applicationContext.getBean("a");
         System.out.println(a);
@@ -22,7 +22,7 @@ public class MainTest {
 
     @Test
     public void root2() {
-        // 单例setter方式
+        // 单例setter方式（Spring通过singletonsCurrentlyInCreation判断是否循环依赖）
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/test/circular/root2.xml");
         StudentA a = (StudentA) applicationContext.getBean("a");
         System.out.println(a);
@@ -30,6 +30,7 @@ public class MainTest {
 
     @Test
     public void root3() {
+        // scope为prototype的bean也无法解决循环依赖的问题（JVM是怎么处理循环依赖的呢？）
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/test/circular/root3.xml");
         StudentA a = (StudentA) applicationContext.getBean("a");
         System.out.println(a);
